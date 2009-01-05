b2280b1242c13ab005e42acf4c6156b71e0c345a
pstieber <pstieber@96ce424f-0a23-4c7b-8751-9736cf04b3c0>
2009-01-05 05:07:38 +0000

    Removed the VC9 specific make-VC9.bat file and added code to make.bat to look for the VC9 version of the DLL, then the VC8 version.

--- portmidi/trunk/pm_java/make.bat
+++ portmidi/trunk/pm_java/make.bat
@@ -1,16 +1,54 @@
+@echo off
+
+rem Compile the java PortMidi interface classes.
 javac jportmidi/*.java
+
+rem Compile the pmdefaults application.
 javac -classpath . pmdefaults/*.java
-rem -- temporarily copy portmusic_logo.png here to add to jar file
+
+rem Temporarily copy the portmusic_logo.png file here to add to the jar file.
 copy pmdefaults\portmusic_logo.png .
+
+rem Create a directory to hold the distribution.
 mkdir win32
-copy ..\release\pmjni.dll win32\pmjni.dll
+
+rem Attempt to copy the interface DLL to the distribution directory.
+
+if exist "pmjni\Release VC9\pmjni.dll" goto copy-vc9-dll
+if exist "..\release\pmjni.dll"  goto copy-vc8-dll
+
+echo "ERROR: pmjni.dll not found!"
+exit /b 1
+
+:copy-vc9-dll
+copy "pmjni\Release VC9\pmjni.dll" win32\pmjni.dll
+goto keepgoing
+
+:copy-vc8-dll
+copy "..\release\pmjni.dll" win32\pmjni.dll
+
+:keepgoing
+
+rem Create a java archive (jar) file of the distribution.
 jar cmf pmdefaults\manifest.txt win32\pmdefaults.jar pmdefaults\*.class portmusic_logo.png jportmidi\*.class
-rem -- clean up temporary image file now that it is in the jar file
+
+rem Clean up the temporary image file now that it is in the jar file.
 del portmusic_logo.png
+
+rem Copy the java execution code obtained from
+rem http://devwizard.free.fr/html/en/JavaExe.html to the distribution
+rem directory.  The copy also renames the file to our desired executable
+rem name.
 copy JavaExe.exe win32\pmdefaults.exe
+
+rem Integrate the icon into the executable using UpdateRsrcJavaExe from
+rem http://devwizard.free.fr
 UpdateRsrcJavaExe -run -exe=win32\pmdefaults.exe -ico=pmdefaults\pmdefaults.ico
+
+rem Copy the 32-bit windows read me file to the distribution directory.
 copy pmdefaults\readme-win32.txt win32\README.txt
+
+rem Copy the license file to the distribution directory.
 copy pmdefaults\pmdefaults-license.txt win32\license.txt
+
 echo "You can run pmdefault.exe in win32"
-
-

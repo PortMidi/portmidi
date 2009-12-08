852be1cb0d3775f2acef381810a87f5e24c27b87
rbd <rbd@96ce424f-0a23-4c7b-8751-9736cf04b3c0>
2009-12-08 01:19:05 +0000

    fixed typo: pm_comon -> pm_common

--- portmidi/trunk/pm_python/build26.sh
+++ portmidi/trunk/pm_python/build26.sh
@@ -3,7 +3,7 @@
 # my copy of Python-2.6.4. Python's make install copies Python.h to
 # /usr/local/include/python2.6/Python.h, but your system may behave
 # differently
-gcc -fPIC -c -o pypm.o -I../pm_comon -I../porttime -I$HOME/Python-2.6.4/Include -I$HOME/Python-2.6.4 pypm.c
+gcc -fPIC -c -o pypm.o -I../pm_common -I../porttime -I$HOME/Python-2.6.4/Include -I$HOME/Python-2.6.4 pypm.c
 # This links pypm.so. It assumes you have compiled libportmidi.so in ../Release
 gcc -shared -o pypm.so  pypm.o -lportmidi -L../Release
 # Install the pypm.so extension. The location should be the "site-packages"
